77f2546080d01e65c9161061203ee41b5d08a485
StackxCodex <siennastackx@gmail.com>
2026-06-13 07:00:46 -0700

    devtag

---
+++ developer_tag.py
@@ -0,0 +1,22 @@
+name = "Shelly Rivers"
+nickname = "StackxCodex"
+career = "Full Stack Developer"
+cloud = "AWS Cloud Engineer"
+
+print("_" * 30)
+print("STACKXCODEX DEVELOPER PROFILE")
+print("_" * 30)
+print()
+print("Name:" + name)
+print("Nickname: " + nickname)
+print("Career: "+ career)
+print("Cloud: " + cloud)
+print("_" * 30)
+print()
+
+print(nickname.upper())
+print(nickname.lower())
+print(len(nickname))
+
+print(nickname.title())
+
